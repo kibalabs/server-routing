ee57067bb9070ffca7197cd7a88d30f0c713b3d5
Krishan Patel <krishan.patel@carbonnv.com>
2020-01-19 22:48:48 +0000

    update run script

--- run.sh
+++ run.sh
@@ -6,11 +6,9 @@ docker network create server-routing || true
 docker build -t server-routing src/
 docker stop server-routing || true
 docker rm server-routing || true
-containerId=$(docker run \
+docker run \
     --detach \
     --name server-routing \
     --publish 80:80 \
     --network server-routing \
     server-routing
-)
-docker logs -f $containerId

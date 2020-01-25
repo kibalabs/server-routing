de48f790a258c346cbac31700f95b2d116622cdb
Krishan Patel <krishan.patel@carbonnv.com>
2020-01-25 21:34:57 +0000

    Use nginx-proxy instead of own + use ssl

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e -o pipefail
 
-docker network create server-routing || true
-
 docker build -t server-routing src/
 docker stop server-routing || true
 docker rm server-routing || true
@@ -10,5 +8,18 @@ docker run \
     --detach \
     --name server-routing \
     --publish 80:80 \
-    --network server-routing \
+    --publish 443:443 \
+    --restart on-failure \
+    --volume /etc/nginx/certs \
+    --volume /etc/nginx/vhost.d \
+    --volume /usr/share/nginx/html \
+    --volume /var/run/docker.sock:/tmp/docker.sock:ro \
     server-routing
+
+docker run \
+    --detach \
+    --name server-routing-letsencrypt \
+    --volumes-from server-routing \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --env "DEFAULT_EMAIL=krishan@whitemonkeylabs.com" \
+    jrcs/letsencrypt-nginx-proxy-companion
